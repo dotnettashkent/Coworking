cf1297c5b916e547cdf79f26079c13c40cc3016d
Saeed <dotnettashkent@gmail.com>
2023-05-07 00:22:39 +0500

    Added CoworkingService class

--- Coworking.Service/Services/CoworkingService.cs
+++ Coworking.Service/Services/CoworkingService.cs
@@ -1,33 +1,70 @@
-﻿using Coworking.Service.DTOs;
+﻿using AutoMapper;
+using Coworking.Data.IRepositories;
+using Coworking.Domain.Entities;
+using Coworking.Service.DTOs;
+using Coworking.Service.Exceptions;
 using Coworking.Service.Interfaces;
 
 namespace Coworking.Service.Services
 {
     public class CoworkingService : ICoworkingService
     {
-        public ValueTask<CoworkingResultDto> CreateAsync(CoworkingCreationDto dto)
+        private readonly IRepository<Coworkingg> repository;
+        private readonly IMapper mapper;
+
+        public CoworkingService(IMapper mapper, IRepository<Coworkingg> repository)
         {
-            throw new NotImplementedException();
+            this.mapper = mapper;
+            this.repository = repository;
         }
 
-        public ValueTask<bool> DeleteAsync(long id)
+        public async ValueTask<CoworkingResultDto> CreateAsync(CoworkingCreationDto dto)
         {
-            throw new NotImplementedException();
+            var example = mapper.Map<Coworkingg>(dto);
+            example = await this.repository.InsertAsync(example);
+            return mapper.Map<CoworkingResultDto>(example);
         }
 
-        public ValueTask<IEnumerable<CoworkingResultDto>> GetAllAsync()
+        public async ValueTask<bool> DeleteAsync(long id)
         {
-            throw new NotImplementedException();
+            var example = await this.repository.GetAsync(id);
+
+            if (example != null) 
+            {
+                await this.repository.DeleteAsync(example);
+                return true;
+            }
+            return false;
+            throw new CoworkingException(404,"Not found...");
         }
 
-        public ValueTask<CoworkingResultDto> GetByIdAsync(long id)
+        public async ValueTask<IEnumerable<CoworkingResultDto>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var examples = await this.repository.GetAllAsync();
+            return mapper.Map<IEnumerable<CoworkingResultDto>>(examples);
         }
 
-        public ValueTask<CoworkingResultDto> UpdateAsync(CoworkingUpdateDto dto)
+        public async ValueTask<CoworkingResultDto> GetByIdAsync(long id)
         {
-            throw new NotImplementedException();
+            var first = await this.repository.GetAsync(id);
+            if(first == null)
+                throw new CoworkingException(404,$"Could not find {id}");
+
+            return mapper.Map<CoworkingResultDto>(first);
+        }
+
+        public async ValueTask<CoworkingResultDto> UpdateAsync(CoworkingUpdateDto dto)
+        {
+            var exaple = await this.repository.GetAsync(dto.Id);
+            if(exaple == null)
+            {
+                return null;
+                throw new CoworkingException(404, "Not found...");
+            }
+
+            mapper.Map(dto, exaple);
+            exaple = await this.repository.UpdateAsync(exaple);
+            return mapper.Map<CoworkingResultDto>(exaple);
         }
     }
 }
