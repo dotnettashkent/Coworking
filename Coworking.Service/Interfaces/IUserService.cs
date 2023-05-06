46a1b166d07a23ac6df4ec84437bebf3dbf215ab
Saeed <dotnettashkent@gmail.com>
2023-05-06 23:54:13 +0500

    Added IUserService interface

--- Coworking.Service/Interfaces/IUserService.cs
+++ Coworking.Service/Interfaces/IUserService.cs
@@ -1,6 +1,13 @@
-﻿namespace Coworking.Service.Interfaces
+﻿using Coworking.Service.DTOs;
+
+namespace Coworking.Service.Interfaces
 {
     public interface IUserService
     {
+        ValueTask<UserResultDto> CreateAsync(UserCreationDto dto);
+        ValueTask<UserResultDto> UpdateAsync(UserUpdateDto dto);
+        ValueTask<bool> DeleteAsync(long id);
+        ValueTask<UserResultDto> GetByIdAsync(long id);
+        ValueTask<IEnumerable<UserResultDto>> GetAllAsync();
     }
 }
