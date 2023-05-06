e22b72cb5944a66724645488372e91c836db4101
Saeed <dotnettashkent@gmail.com>
2023-05-07 00:24:40 +0500

    Added Mapping

--- Coworking.Service/Mappers/MappingProfile.cs
+++ Coworking.Service/Mappers/MappingProfile.cs
@@ -1,8 +1,24 @@
 ﻿using AutoMapper;
+using Coworking.Domain.Entities;
+using Coworking.Service.DTOs;
 
 namespace Coworking.Service.Mappers
 {
     public class MappingProfile : Profile
     {
+        public MappingProfile() 
+        {
+            //User
+
+            CreateMap<User,UserCreationDto>().ReverseMap();
+            CreateMap<User,UserResultDto>().ReverseMap();
+            CreateMap<User,UserUpdateDto>().ReverseMap();
+
+            //Coworking
+
+            CreateMap<Coworkingg,CoworkingCreationDto>().ReverseMap();
+            CreateMap<Coworkingg,CoworkingUpdateDto>().ReverseMap();
+            CreateMap<Coworkingg,CoworkingResultDto>().ReverseMap();
+        }
     }
 }
